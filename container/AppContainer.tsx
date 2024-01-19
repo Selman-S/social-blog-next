b986d39bde396cdd5c4d8b2da3abd4f8006872ea
selman <gunduzgece546@gmail.com>
2024-01-19 23:52:59 +0300

    Remove unused imports and commented code

--- container/AppContainer.tsx
+++ container/AppContainer.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { selectThemeConfig, themeSlice } from '@/lib/redux/slices/themeSlice';
-import { selectUser } from '@/lib/redux/slices/userSlice';
 import useAuthCall from '@/service/useAuthCall';
 import { PropsWithChildren, useEffect } from 'react';
 
@@ -10,19 +9,10 @@ import { useDispatch, useSelector } from 'react-redux';
 function AppContainer({ children }: PropsWithChildren) {
 
     const themeConfig = useSelector(selectThemeConfig)
-    const { userObserver } = useAuthCall();
-
-    // const themeConfig = useSelector((state: IRootState) => state.themeConfig);
     const dispatch = useDispatch();
-    // const { i18n } = useTranslation();
 
     useEffect(() => {
-        const user = userObserver()
-        console.log(user.currentUser);
-
         dispatch(themeSlice.actions.toggleTheme(localStorage.getItem('theme') || themeConfig.theme));
-
-
 
     }, [dispatch, themeConfig.theme]);
 
