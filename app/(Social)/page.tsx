c07011cd608f2ae56bb4c23ea9fd0e89b875eb93
selman <gunduzgece546@gmail.com>
2024-01-20 11:04:42 +0300

    Refactor page and layout components

--- app/(Social)/page.tsx
+++ app/(Social)/page.tsx
@@ -5,25 +5,12 @@ import Sponsored from "../components/homepage/Sponsored"
 
 export default async function Home() {
 
-
-
-
-
-
   return (
     <>
       <div className=" flex flex-nowrap gap-5 mx-auto justify-between max-w-[1960px] ">
-
         <UserList />
-
-
-
-
         <PostList />
-
-
         <Sponsored />
-
       </div>
 
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,18 +19,20 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  await reduxStore.dispatch(getPostsWithThunk())
-  await reduxStore.dispatch(getUsersWithThunk())
-  const postState = reduxStore.getState().post.data
-  const userState = reduxStore.getState().user.data
+  // await reduxStore.dispatch(getPostsWithThunk())
+  // await reduxStore.dispatch(getUsersWithThunk())
+  // const postState = reduxStore.getState().post.data
+  // const userState = reduxStore.getState().user.data
 
   return (
     <ReduxProviders preloadedState={{
       post: {
-        data: postState
+        data: []
+        // data: postState 
       },
       user: {
-        data: userState
+        data: []
+        // data: userState
       }
     }}>
 
