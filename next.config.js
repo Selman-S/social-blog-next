ecc67e68ddc77c8c5159d63dc095522ea2b22fad
selman <gunduzgece546@gmail.com>
2024-01-21 16:46:24 +0300

    Add CDN configuration for Facebook content

--- next.config.js
+++ next.config.js
@@ -32,6 +32,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fesb4-1.fna.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
